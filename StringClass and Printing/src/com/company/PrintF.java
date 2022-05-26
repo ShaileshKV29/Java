1727f53e1128207145e5220c5a4b10fda137314b
ShaileshKV29 <shailesh29062001@gmail.com>
2022-05-26 21:42:02 +0530

    Printf Method

---
+++ StringClass and Printing/src/com/company/PrintF.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class PrintF {
+    public static void main(String[] args) {
+        System.out.printf("Age is %e\n", 20.4d);
+        System.out.printf("%2$d %3$d %1$d", 10, 20, 30);
+    }
+}
