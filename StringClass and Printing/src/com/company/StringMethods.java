a6d5a306eba4f3a62d7e2d65997c880416ddcdec
ShaileshKV29 <shailesh29062001@gmail.com>
2022-05-27 22:23:03 +0530

    String Methods

---
+++ StringClass and Printing/src/com/company/StringMethods.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class StringMethods {
+    public static void main(String[] args) {
+        String str = "    Welcome    ";
+        str = str.trim();
+        System.out.println(str);
+        String str1 = str.substring(2, 3);
+        System.out.println(str1);
+
+        String str2 = "Shailesh Kumar Verma";
+        System.out.println(str2.startsWith("Shailesh"));
+        System.out.println(str2.endsWith("Verma"));
+
+        String str3 = String.valueOf(10.5f+20);
+        System.out.println(str3);
+    }
+}
