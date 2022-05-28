db80d8b98ae74c66e125e4d8a791788fba53ee93
ShaileshKV29 <shailesh29062001@gmail.com>
2022-05-28 22:34:54 +0530

    Equals Method

---
+++ StringClass and Printing/src/com/company/Equals.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Equals {
+    public static void main(String[] args) {
+        String s1 = "Shailesh";
+        String s2 = "shailesh";
+        System.out.println(s1.equals(s2));
+    }
+}
