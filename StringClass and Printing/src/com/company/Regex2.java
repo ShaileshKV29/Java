d138bf9db43c58020e00f0069ff08d6170dcecd2
ShaileshKV29 <shailesh29062001@gmail.com>
2022-06-03 23:12:25 +0530

    Regular Expression 2

---
+++ StringClass and Printing/src/com/company/Regex2.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Regex2 {
+    public static void main(String[] args) {
+        String str = "aabbcc";
+        System.out.println(str.matches("[abc]*"));
+    }
+}
