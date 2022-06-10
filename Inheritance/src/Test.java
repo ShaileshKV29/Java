0c7a1c1dee2d2b175afe77a44b178e978a765e19
ShaileshKV29 <shailesh29062001@gmail.com>
2022-06-10 12:38:35 +0530

    Test

--- Inheritance/src/Test.java
+++ Inheritance/src/Test.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Test {
+class Circle
+{
+    public int radius;
+    public Circle()
+    {
+        radius = 0;
+    }
+    public double area()
+    {
+        return radius*radius*3.14;
+    }
+
+    public double perimeter()
+    {
+        return 2*3.13*radius;
+    }
+}
+
+class Cylinder extends Circle
+{
+    public int height;
+    public Cylinder()
+    {
+        height = 0;
+    }
+
+    public double volume()
+    {
+        return 3.14*radius*radius*height;
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
 }
