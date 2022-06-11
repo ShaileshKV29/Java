aae4e5a702e43c198173e9b22d114ddfbb3f56ac
ShaileshKV29 <shailesh29062001@gmail.com>
2022-06-11 11:23:20 +0530

    Abstract CLassses

---
+++ Inheritance/src/AbstractClasses.java
@@ -0,0 +1,19 @@
+abstract class Super2
+{
+    abstract void method();
+}
+
+class Sub2 extends Super2
+{
+    void method()
+    {
+        System.out.println("Method of Sub");
+    }
+}
+
+
+public class AbstractClasses {
+    public static void main(String[] args) {
+
+    }
+}
